ff34e7ca44a8296595a8c59562e3a8b6d7ce7ea4
Pablo <uo265314@uniovi.es>
2023-04-17 23:07:53 +0200

    Automatic profile pic display

--- webapp/src/components/NavBar.tsx
+++ webapp/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react';
+import { useState} from 'react';
 import { Link } from "react-router-dom";
 import LoginForm from './login/LoginForm';
 import { Stack, Box, Button } from '@mui/material';
@@ -38,20 +38,18 @@ export const NavBar = () => {
         } 
     }
 
-    function searchProfileImg(webId: string): string | undefined {
+    function searchProfileImg(webId: string|undefined) {
         let url = "/user.png"
         if (webId) {
             findPersonData(webId)
               .then(personData => {
-                console.log("Imagen de perfil: " + personData.photo)
-                url = personData.photo;
-                setProfilePic(url)
+                // console.log("Imagen de perfil: " + personData.photo)
+                setProfilePic(personData.photo)
             })
             .catch(error => {
                 console.error("Error en findPersonData:", error);
               });
-          }
-        return url
+        }
     }
 
     return (
@@ -72,7 +70,8 @@ export const NavBar = () => {
 
                         <Stack direction={{ xs: 'column', sm: 'row' }} alignItems='center' sx={{ flexGrow: '2' }} justifyContent='flex-end' spacing={{ xs: 1, sm: 2, md: 4 }}>
                             <Box component="p" color={'white'}>{session.info.webId?.substring(8).split('.')[0]}</Box>
-                            <a href={searchProfileImg(session.info.webId?session.info.webId:"")} target="_blank" rel="noopener noreferrer">
+                            {searchProfileImg(session.info.webId)}
+                            <a href={profilePic} target="_blank" rel="noopener noreferrer">
                                 <img src={profilePic} alt="profile pic" className="profile-pic" />
                             </a>
                             <LogoutButton>
