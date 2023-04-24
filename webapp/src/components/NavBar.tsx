b7150b47897c100a2451b9f9523bdab726eee4ce
Pablo <uo265314@uniovi.es>
2023-04-24 02:02:39 +0200

    Nav component refactor

--- webapp/src/components/NavBar.tsx
+++ webapp/src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import LoginForm from './login/LoginForm';
 import { Stack, Box, Button, Select, MenuItem } from '@mui/material';
 import { useSession, LogoutButton } from '@inrupt/solid-ui-react';
 import { useTranslation } from 'react-i18next';
-import { findPersonData } from '../helpers/ProfileHelper';
+import { PersonData, findPersonData } from '../helpers/ProfileHelper';
 
 export const NavBar = () => {
     const UK_URL = "/uk-flag.png";
@@ -18,7 +18,7 @@ export const NavBar = () => {
 
     const { t, i18n } = useTranslation("translation");
 
-    const [profilePic, setProfilePic] = useState("/user.png")
+    const [personData, setPersonData] = useState<PersonData>()
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -32,12 +32,12 @@ export const NavBar = () => {
         i18n.changeLanguage(lang);
     }
 
-    function searchProfileImg(webId: string|undefined) {
+    function searchPersonData(webId: string|undefined) {
+        // let name = session.info.webId?.substring(8).split('.')[0]
         if (webId) {
             findPersonData(webId)
               .then(personData => {
-                    // console.log("Imagen de perfil: " + personData.photo)
-                    setProfilePic(personData.photo || DEFAULT_USERPIC)
+                setPersonData(personData)
             })
             .catch(error => {
                 console.error("Error en findPersonData:", error);
@@ -45,67 +45,96 @@ export const NavBar = () => {
         }
     }
 
+    function getImg() {
+        if (personData){
+            return personData.photo;
+        } else {
+            return DEFAULT_USERPIC;
+        }
+    }
+
     return (
         <nav>
             <Stack
                 direction={{ xs: 'column', sm: 'row' }}
-                spacing={{ xs: 1, sm: 2, md: 4 }}
+                spacing={{ xs: 1, sm: 2, md: 0 }}
                 alignItems='center'
-                justifyContent='left'
+                justifyContent='center'
+                sx={{
+                    display:"grid",
+                    gridTemplateColumns: "1fr auto 1fr",
+                    textAlign: "center"
+                }}
             >
-                <Link to="/"><img src="/logo-no-background.png" className="App-logo" alt="logo" height="60" /></Link>
-                <Link to="/map">{t("NavBar.map" as const)}</Link>
-                { session.info.isLoggedIn ? <></> : 
-                    <>
-                        <Link to="/aboutus">{t("NavBar.about" as const)}</Link>     
-                        <Select value={i18n.language} onChange={(e) => changeLanguage(e.target.value)}
+                <Box>
+                    <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    spacing={{ xs: 1, sm: 2, md: 4 }}
+                    alignItems='center'
+                    justifyContent='left'
+                    >
+                        <Link to="/"><img src="/logo-no-background.png" className="App-logo" alt="logo" height="60" /></Link>
+                        <Link to="/map">{t("NavBar.map" as const)}</Link>
+                        {session.info.isLoggedIn ? <>
+                            <Link to="/ubications">{t("NavBar.ubic")}</Link>
+                            <Link to="/friends">{t("NavBar.friends")}</Link>
+                        </> : <></> 
+                        }
+                        <Link to="/aboutus">{t("NavBar.about" as const)}</Link>
+                    </Stack>
+                </Box>
+
+                <Box>
+                    <Select value={i18n.language} onChange={(e) => changeLanguage(e.target.value)}
                             sx={{boxShadow: 'none', '.MuiOutlinedInput-notchedOutline': { border: 0 } }}>
                             <MenuItem value={"en"}> <img src={UK_URL} height="35" alt="en_icon" /> </MenuItem>
                             <MenuItem value={"es"}> <img src={ES_URL} height="35" alt="es_icon" /> </MenuItem>
-                        </Select>   
-                        {/* <Button onClick={changeLanguage} style={{marginRight:"2.5%", height:"40" }}>
-                            <img src={icon} height="40" alt="language" />
-                        </Button> */}
-                    </>                    
-                }
+                    </Select>
+                </Box>
 
-                { session.info.isLoggedIn ? 
-                    <>
-                        <Link to="/ubications">{t("NavBar.ubic")}</Link>
-                        <Link to="/friends">{t("NavBar.friends")}</Link>
-                        <Link to="/aboutus">{t("NavBar.about" as const)}</Link>
-                        <Select value={i18n.language} onChange={(e) => changeLanguage(e.target.value)}
-                            sx={{boxShadow: 'none', '.MuiOutlinedInput-notchedOutline': { border: 0 } }}>
-                            <MenuItem value={"en"}><img src={UK_URL} height="40" alt="en_icon" /></MenuItem>
-                            <MenuItem value={"es"}><img src={ES_URL} height="40" alt="es_icon" /></MenuItem>
-                        </Select>
+                <Box>
+                    {session.info.isLoggedIn ? <>
+                        {searchPersonData(session.info.webId)}
+                        <Stack 
+                        direction={{ xs: 'column', sm: 'row' }} 
+                        alignItems='center'
+                        justifyContent='flex-end' 
+                        spacing={{ xs: 1, sm: 2, md: 2 } }
+                        margin="1em">
 
-                        {/* <Button onClick={changeLanguage} style={{marginRight:"2.5%", height:"40" }}>
-                            <img src={icon} height="40" alt="language" />
-                        </Button>   */}
-
-                        <Stack direction={{ xs: 'column', sm: 'row' }} alignItems='center' sx={{ flexGrow: '2' }} justifyContent='flex-end' spacing={{ xs: 2, sm: 2, md: 2 } }>
-                            <Box component="p" color={'white'}>{session.info.webId?.substring(8).split('.')[0]}</Box>
-                            {searchProfileImg(session.info.webId)}
-                            <a href={session.info.webId} target="_blank" rel="noopener noreferrer">
-                                <img src={profilePic} alt="profile pic" className="profile-pic" />
+                            <Box component="p" color={'white'}>
+                                {personData?.name}
+                            </Box>
+                            <a href={personData?.photo} target="_blank" rel="noopener noreferrer">
+                                <img src={getImg()} alt="" className="profile-pic" />
                             </a>
                             <LogoutButton>
                                 <Button variant="outlined" sx={{ margin: "1em", marginLeft: "0em", color:'lightblue', border: '2px solid' }}>
                                     {t("NavBar.close")}
                                 </Button>
                             </LogoutButton>
+
                         </Stack>
-                    </>
-                    : <Stack direction={{ xs: 'column', sm: 'row' }} alignItems='center' sx={{ flexGrow: '2' }} justifyContent='flex-end' spacing={{ xs: 1, sm: 2, md: 4 }}>
-                        <Button variant="outlined" onClick={handleClickOpen} sx={{ margin: "1em", color:'lightblue', border: '2px solid', marginRight: '3.5%' }}>
+                    </> : <>
+                        <Stack
+                        direction={{ xs: 'column', sm: 'row' }}
+                        alignItems='center' 
+                        sx={{ flexGrow: '2' }} 
+                        justifyContent='flex-end' 
+                        margin="1em"
+                        spacing={{ xs: 1, sm: 2, md: 2 }}>
+                            <img src={DEFAULT_USERPIC} alt="" className="profile-pic" />
+                            <Button variant="outlined" onClick={handleClickOpen} sx={{ margin: "1em", color:'lightblue', border: '2px solid', marginRight: '3.5%' }}>
                             {t("NavBar.open")}
                         </Button>
-                        <LoginForm
-                            open={open}
-                            onClose={handleClose}
-                        />
-                    </Stack>}                  
+                            <LoginForm
+                                open={open}
+                                onClose={handleClose}
+                            />
+                        </Stack>
+                    </>}
+
+                </Box>               
             </Stack>
         </nav>
     )
