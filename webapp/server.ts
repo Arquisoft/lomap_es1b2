42b202912608085cbc650555111854498f7facf6
pelotazos <chispablos@gmail.com>
2023-05-01 21:17:07 +0200

    fix server

--- webapp/server.ts
+++ webapp/server.ts
@@ -1,12 +1,19 @@
 import express,{Application} from 'express'; 
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
-
+import path from 'path';
 var app: Application = express()
 
 const port: number = 80;
 
+
 app.use(express.static('build'))
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, ():void => {
     console.log('Webapp started on port '+ port);
